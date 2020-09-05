1d602f52817767989c3d93406af7595443f0bff7
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-09-04 20:24:15 -0400

    upate menu items with routing

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from '../../pages/HomePage/homepage.component';
 
@@ -6,8 +7,26 @@ import './App.styles.scss';
 
 const App = () => (
 	<div>
-		<HomePage />
+		<Switch>
+			<Route exact path="/" component={HomePage} />
+		</Switch>
 	</div>
 );
 
 export default App;
+
+/*
+Switch - when switch sees something match a path, It will only render that component
+				 prevents us from accidently rendering multiple components
+
+!!! When using react-router, each component rendered is passed three props:
+			history, location, and match
+
+			the match prop is important because it allows us to access the params of the url
+					example:  /clothing/jackets/:jacketId
+				in the component we will have acces to the param jacketId
+				we also have access to match.url which will give our current location
+					useful for dynamic routing
+
+			the history prop gives us access to history.push('where we want to go')
+*/
